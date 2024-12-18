618d38e34f0a2177382ec5cb5ab14d6de2d12743
justdev <mhshin71@gmail.com>
2024-12-18 23:25:23 +0900

    simple fix

--- tutorial_image_bounding.py
+++ tutorial_image_bounding.py
@@ -1,33 +1,52 @@
 import json
 import os
 import re
+import sys
+import PIL.Image as pil
 import google.generativeai as genai
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
+if len(sys.argv) != 3:
+    sys.exit(1)
+    
+filenm = sys.argv[1]
+obj = sys.argv[2]
+
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel(model_name='gemini-1.5-flash')
 
-import PIL.Image as pil
-goats = pil.open("./cats.png")
-width, height = goats.size
-
-prompt = "Return bounding boxes around every goat, " \
+imgobj = pil.open(filenm)
+width, height = imgobj.size
+if imgobj.format == "JPEG":
+    postfix = "jpg"
+elif imgobj.format == "PNG":
+    postfix = "png"
+elif imgobj.format == "GIF":
+    postfix = "gif"
+elif imgobj.format == "BMP":
+    postfix = "bmp"
+else:
+    postfix = ""
+    
+prompt = f"Return bounding boxes around every {obj}, " \
          "for each one return [ymin, xmin, ymax, xmax]"
-response = model.generate_content([goats, prompt])
+response = model.generate_content([imgobj, prompt])
 print(response.text)
 
 def extract_bounding_boxes(text):
     # Regular expression to match numbers inside square brackets
-    pattern = r"\[?\d+[,][ ]?\d+[,][ ]?\d+[,][ ]?\d+\]?"
+    pattern = r"\[?\d+[, ]+\d+[, ]+\d+[, ]+\d+\]?"
     matches = re.findall(pattern, text)
     #print(matches)
     
     bounding_boxes = []
     for match in matches:
+        if match.find(',') == -1:
+            match = match.replace(' ', ',')
         if match[0] == '[' and match[-1] == ']':
             bounding_boxes.append(json.loads(match))
         else:
@@ -47,8 +66,8 @@ try:
         lower = (item[2] / 1000) * height
         left = (item[1] / 1000) * width
         upper = (item[0] / 1000) * height
-        cropped_img = goats.crop((left, upper, right, lower))
-        with open(f'./cat-{index}.png', 'wb') as fp:
+        cropped_img = imgobj.crop((left, upper, right, lower))
+        with open(f'./{obj}-{index}.{postfix}', 'wb') as fp:
             cropped_img.save(fp)
         index += 1
 
